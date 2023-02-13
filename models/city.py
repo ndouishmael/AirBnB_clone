c702ffee21cbdea1a188064008f1438dc99fd4ba
Rotondwa Ishmael Ndou <111059267+ndouishmael@users.noreply.github.com>
2023-02-13 12:46:42 +0200

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-"""Defines the City class."""
+"""City module"""
 from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """Represent a city.
-    
+    """class City
     Attributes:
-        state_id (str): The state id.
-        name (str): The name of the city.
+        state_id (str) State ID.
+        name (str): City name.
     """
-
     state_id = ""
     name = ""
